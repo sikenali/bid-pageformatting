4550814c2ccda1f95655e7ecb10bb814ce049100
sikenali <mrchiang1015@outlook.com>
2026-06-30 12:40:24 +0800

    feat(backend): fix TOC tab leader and level-style handling

--- backend/format_toc.go
+++ backend/format_toc.go
@@ -3,12 +3,17 @@ package wordformat
 import (
 	"fmt"
 
+	"github.com/unidoc/unioffice/color"
 	"github.com/unidoc/unioffice/measurement"
 
 	"github.com/unidoc/unioffice/document"
 )
 
 func ApplyTOCFormat(doc *document.Document, toc *TOCConfig) error {
+	if toc == nil || !toc.Enable {
+		return nil
+	}
+
 	for _, p := range doc.Paragraphs() {
 		styleID := p.Style()
 
@@ -22,10 +27,13 @@ func ApplyTOCFormat(doc *document.Document, toc *TOCConfig) error {
 					}
 					rPr.SetFontFamily(font)
 					if toc.TitleCNFont != "" && toc.TitleCNFont != toc.TitleENFont {
-						rPr.X().RFonts.EastAsiaAttr = &toc.TitleCNFont
+						cn := toc.TitleCNFont
+						rPr.X().RFonts.EastAsiaAttr = &cn
 					}
 				}
-				rPr.SetSize(cnSizeToHalfPoints(toc.TitleSizeCN) * measurement.HalfPoint)
+				if toc.TitleSizeCN != "" {
+					rPr.SetSize(cnSizeToHalfPoints(toc.TitleSizeCN) * measurement.HalfPoint)
+				}
 			}
 			continue
 		}
@@ -33,6 +41,31 @@ func ApplyTOCFormat(doc *document.Document, toc *TOCConfig) error {
 		for i, ls := range toc.LevelStyles {
 			if styleID == fmt.Sprintf("toc%d", i+1) ||
 				styleID == fmt.Sprintf("TOC%d", i+1) {
+
+				if ls.Align != "" {
+					p.SetAlignment(alignFromString(ls.Align))
+				}
+				if ls.LineSpacingMode != "" {
+					p.SetLineSpacing(
+						twipsFromLineSpacing(ls.LineSpacingMode, ls.LineSpacingValue)*measurement.Twips,
+						lineSpacingEnum(ls.LineSpacingMode))
+				}
+				if ls.SpaceBeforeValue > 0 {
+					p.SetBeforeSpacing(twipsFromSpacing(ls.SpaceBeforeValue, ls.SpaceBeforeUnit) * measurement.Twips)
+				}
+				if ls.SpaceAfterValue > 0 {
+					p.SetAfterSpacing(twipsFromSpacing(ls.SpaceAfterValue, ls.SpaceAfterUnit) * measurement.Twips)
+				}
+				if ls.FirstLineIndentChars > 0 {
+					p.SetFirstLineIndent(measurement.Distance(ls.FirstLineIndentChars*200) * measurement.Twips)
+				}
+				if ls.LeftIndentChars > 0 {
+					p.SetLeftIndent(measurement.Distance(ls.LeftIndentChars*200) * measurement.Twips)
+				}
+				if ls.RightIndentValue > 0 {
+					p.SetRightIndent(twipsFromIndentValue(ls.RightIndentValue, ls.RightIndentUnit) * measurement.Twips)
+				}
+
 				for _, r := range p.Runs() {
 					rPr := r.Properties()
 					if ls.ENFont != "" || ls.CNFont != "" {
@@ -42,18 +75,24 @@ func ApplyTOCFormat(doc *document.Document, toc *TOCConfig) error {
 						}
 						rPr.SetFontFamily(font)
 						if ls.CNFont != "" && ls.CNFont != ls.ENFont {
-							rPr.X().RFonts.EastAsiaAttr = &ls.CNFont
+							cn := ls.CNFont
+							rPr.X().RFonts.EastAsiaAttr = &cn
 						}
 					}
-					rPr.SetSize(cnSizeToHalfPoints(ls.SizeCN) * measurement.HalfPoint)
+					if ls.SizeCN != "" {
+						rPr.SetSize(cnSizeToHalfPoints(ls.SizeCN) * measurement.HalfPoint)
+					}
 					rPr.SetBold(ls.Bold)
+					if ls.Italic {
+						rPr.SetItalic(true)
+					}
 					if ls.ColorRGB != [3]int{0, 0, 0} {
 						hex := fmt.Sprintf("%02X%02X%02X", ls.ColorRGB[0], ls.ColorRGB[1], ls.ColorRGB[2])
 						rPr.SetColor(colorFromHex(hex))
 					}
-				}
-				if ls.LeftIndentChars > 0 {
-					p.SetLeftIndent(measurement.Distance(ls.LeftIndentChars*200) * measurement.Twips)
+					if ls.TabLeader != "" {
+						rPr.SetUnderline(underlineFromType("none"), color.FromHex("#000000"))
+					}
 				}
 			}
 		}
