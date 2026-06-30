28f2e280d96f52f2869dc256881fc94e7aa496c4
sikenali <mrchiang1015@outlook.com>
2026-06-30 13:18:14 +0800

    feat(backend): add InsertCJKEUSpacingWith configurable space count
    Add InsertCJKEUSpacingWith that inserts 1-4 configurable spaces
    (bounded) between CJK and non-CJK characters. The existing
    InsertCJKEUSpacing now delegates to it with a default of 1 space.
    Skip run modification when no changes would be made (optimization).

--- backend/preprocess_spaces.go
+++ backend/preprocess_spaces.go
@@ -2,21 +2,35 @@ package wordformat
 
 import (
 	"regexp"
+	"strings"
 
 	"github.com/unidoc/unioffice/document"
 )
 
 func InsertCJKEUSpacing(doc *document.Document) error {
+	return InsertCJKEUSpacingWith(doc, 1)
+}
+
+func InsertCJKEUSpacingWith(doc *document.Document, spaceCount int) error {
+	if spaceCount < 1 {
+		spaceCount = 1
+	}
+	if spaceCount > 4 {
+		spaceCount = 4
+	}
+	sp := strings.Repeat(" ", spaceCount)
 	re1 := regexp.MustCompile(`([\p{Han}])([a-zA-Z0-9])`)
 	re2 := regexp.MustCompile(`([a-zA-Z0-9])([\p{Han}])`)
 
 	for _, p := range doc.Paragraphs() {
 		for _, r := range p.Runs() {
 			oldText := r.Text()
-			newText := re1.ReplaceAllString(oldText, "${1} ${2}")
-			newText = re2.ReplaceAllString(newText, "${1} ${2}")
-			r.ClearContent()
-			r.AddText(newText)
+			newText := re1.ReplaceAllString(oldText, "${1}"+sp+"${2}")
+			newText = re2.ReplaceAllString(newText, "${1}"+sp+"${2}")
+			if newText != oldText {
+				r.ClearContent()
+				r.AddText(newText)
+			}
 		}
 	}
 	return nil
