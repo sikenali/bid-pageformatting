2e2ae6723d9aeff1fa96b45fa88286720654f570
sikenali <mrchiang1015@outlook.com>
2026-06-30 12:39:59 +0800

    feat(backend): apply caption formatting for fig/tbl captions

--- backend/format_captions.go
+++ backend/format_captions.go
@@ -4,13 +4,11 @@ import (
 	"regexp"
 
 	"github.com/unidoc/unioffice/document"
-	"github.com/unidoc/unioffice/measurement"
-	"github.com/unidoc/unioffice/schema/soo/wml"
 )
 
 func ApplyCaptionFormats(doc *document.Document, fig, tbl *CaptionStyle) error {
-	reFig := regexp.MustCompile(`^[图圖]`)
-	reTbl := regexp.MustCompile(`^表`)
+	reFig := regexp.MustCompile(`^[图圖]\s*\d`)
+	reTbl := regexp.MustCompile(`^表\s*\d`)
 
 	for _, p := range doc.Paragraphs() {
 		text := paragraphText(p)
@@ -23,29 +21,20 @@ func ApplyCaptionFormats(doc *document.Document, fig, tbl *CaptionStyle) error {
 		default:
 			continue
 		}
+		if style == nil {
+			continue
+		}
 
-		p.SetAlignment(wml.ST_JcCenter)
-		p.SetFirstLineIndent(0)
-		p.SetLineSpacing(
-			twipsFromLineSpacing(style.LineSpacingMode, style.LineSpacingValue)*measurement.Twips,
-			lineSpacingEnum(style.LineSpacingMode))
-		p.SetBeforeSpacing(twipsFromSpacing(style.SpaceBeforeValue, style.SpaceBeforeUnit) * measurement.Twips)
-		p.SetAfterSpacing(twipsFromSpacing(style.SpaceAfterValue, style.SpaceAfterUnit) * measurement.Twips)
+		if style.Align != "" {
+			p.SetAlignment(alignFromString(style.Align))
+		} else {
+			p.SetAlignment(alignFromString("CENTER"))
+		}
+
+		applyParagraphSpacing(p, &style.BodyStyle)
 
 		for _, r := range p.Runs() {
-			rPr := r.Properties()
-			if style.ENFont != "" || style.CNFont != "" {
-				font := style.ENFont
-				if font == "" {
-					font = style.CNFont
-				}
-				rPr.SetFontFamily(font)
-				if style.CNFont != "" && style.CNFont != style.ENFont {
-					rPr.X().RFonts.EastAsiaAttr = &style.CNFont
-				}
-			}
-			rPr.SetSize(cnSizeToHalfPoints(style.SizeCN) * measurement.HalfPoint)
-			rPr.SetBold(style.Bold)
+			applyStyleToRun(r, &style.BodyStyle)
 		}
 	}
 	return nil
