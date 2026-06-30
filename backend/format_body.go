d9a992a0c02bea1a26300f3b4f80d5d3531e7f5c
sikenali <mrchiang1015@outlook.com>
2026-06-30 12:39:44 +0800

    feat(backend): add underline and color support to body formatting

--- backend/format_body.go
+++ backend/format_body.go
@@ -1,71 +1,85 @@
 package wordformat
 
 import (
+	"github.com/unidoc/unioffice/color"
 	"github.com/unidoc/unioffice/document"
 	"github.com/unidoc/unioffice/measurement"
 	"github.com/unidoc/unioffice/schema/soo/wml"
 )
 
-func ApplyBodyFormat(doc *document.Document, s *BodyStyle) error {
-	cnSize := cnSizeToHalfPoints(s.SizeCN)
+func applyStyleToRun(r document.Run, s *BodyStyle) {
+	rPr := r.Properties()
+	if s.ENFont != "" || s.CNFont != "" {
+		font := s.ENFont
+		if font == "" {
+			font = s.CNFont
+		}
+		rPr.SetFontFamily(font)
+		if s.CNFont != "" && s.CNFont != s.ENFont {
+			cn := s.CNFont
+			rPr.X().RFonts.EastAsiaAttr = &cn
+		}
+	}
+	if s.SizeCN != "" {
+		rPr.SetSize(cnSizeToHalfPoints(s.SizeCN) * measurement.HalfPoint)
+	}
+	if s.Bold {
+		rPr.SetBold(true)
+	}
+	if s.Italic {
+		rPr.SetItalic(true)
+	}
+	if s.Underline {
+		rPr.SetUnderline(wml.ST_UnderlineSingle, color.FromHex("#000000"))
+	}
+	if s.Color != "" {
+		rPr.SetColor(colorFromHex(s.Color))
+	}
+}
 
+func applyParagraphSpacing(p document.Paragraph, s *BodyStyle) {
+	if s.LineSpacingMode != "" {
+		p.SetLineSpacing(
+			twipsFromLineSpacing(s.LineSpacingMode, s.LineSpacingValue)*measurement.Twips,
+			lineSpacingEnum(s.LineSpacingMode))
+	}
+	if s.SpaceBeforeValue > 0 || s.SpaceBeforeUnit != "" {
+		p.SetBeforeSpacing(twipsFromSpacing(s.SpaceBeforeValue, s.SpaceBeforeUnit) * measurement.Twips)
+	}
+	if s.SpaceAfterValue > 0 || s.SpaceAfterUnit != "" {
+		p.SetAfterSpacing(twipsFromSpacing(s.SpaceAfterValue, s.SpaceAfterUnit) * measurement.Twips)
+	}
+	if s.FirstLineIndentChars > 0 {
+		p.SetFirstLineIndent(measurement.Distance(s.FirstLineIndentChars*200) * measurement.Twips)
+	}
+	if s.LeftIndentValue > 0 {
+		p.SetLeftIndent(twipsFromIndentValue(s.LeftIndentValue, s.LeftIndentUnit) * measurement.Twips)
+	}
+	if s.RightIndentValue > 0 {
+		p.SetRightIndent(twipsFromIndentValue(s.RightIndentValue, s.RightIndentUnit) * measurement.Twips)
+	}
+}
+
+func ApplyBodyFormat(doc *document.Document, s *BodyStyle) error {
 	for _, p := range doc.Paragraphs() {
 		styleID := p.Style()
 		if isHeading(styleID) {
 			continue
 		}
 
-		switch s.Align {
-		case "JUSTIFY":
-			p.SetAlignment(wml.ST_JcBoth)
-		case "LEFT":
-			p.SetAlignment(wml.ST_JcStart)
-		case "CENTER":
-			p.SetAlignment(wml.ST_JcCenter)
-		case "RIGHT":
-			p.SetAlignment(wml.ST_JcEnd)
+		if s.Align != "" {
+			p.SetAlignment(alignFromString(s.Align))
 		}
 
-		if s.FirstLineIndentChars > 0 {
-			p.SetFirstLineIndent(measurement.Distance(s.FirstLineIndentChars*200) * measurement.Twips)
-		}
-		if s.LeftIndentValue > 0 {
-			p.SetLeftIndent(measurement.Distance(s.LeftIndentValue*200) * measurement.Twips)
-		}
-		if s.RightIndentValue > 0 {
-			p.SetRightIndent(measurement.Distance(s.RightIndentValue*200) * measurement.Twips)
-		}
-
-		p.SetLineSpacing(
-			twipsFromLineSpacing(s.LineSpacingMode, s.LineSpacingValue)*measurement.Twips,
-			lineSpacingEnum(s.LineSpacingMode))
-
-		p.SetBeforeSpacing(twipsFromSpacing(s.SpaceBeforeValue, s.SpaceBeforeUnit) * measurement.Twips)
-		p.SetAfterSpacing(twipsFromSpacing(s.SpaceAfterValue, s.SpaceAfterUnit) * measurement.Twips)
+		applyParagraphSpacing(p, s)
 
 		for _, r := range p.Runs() {
-			rPr := r.Properties()
-			if s.ENFont != "" || s.CNFont != "" {
-				font := s.ENFont
-				if font == "" {
-					font = s.CNFont
-				}
-				rPr.SetFontFamily(font)
-				if s.CNFont != "" && s.CNFont != s.ENFont {
-					rPr.X().RFonts.EastAsiaAttr = &s.CNFont
-				}
-			}
-			rPr.SetSize(cnSize * measurement.HalfPoint)
-			rPr.SetBold(s.Bold)
-			if s.Italic {
-				rPr.SetItalic(true)
-			}
+			applyStyleToRun(r, s)
 		}
 	}
 	return nil
 }
 
 func isHeading(styleID string) bool {
-	return len(styleID) >= 7 && styleID[:7] == "Heading" ||
-		len(styleID) >= 3 && styleID[:3] == "TOC"
+	return matchHeadingLevel(styleID) > 0 || (len(styleID) >= 3 && styleID[:3] == "TOC")
 }
